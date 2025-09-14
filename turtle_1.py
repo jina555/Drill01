ae2f8f626cdb2506cec5c90b4471b547d691f243
jina555 <kja390@tukorea.ac.kr>
2025-09-14 15:05:14 +0900

    Update turtle_1.py

--- turtle_1.py
+++ turtle_1.py
@@ -1,36 +1,44 @@
 import turtle
 
 def move_w():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+
+
+def move_s():
+    turtle.setheading(270)
     turtle.forward(50)
     turtle.stamp()
 
 def move_a():
-    turtle.left(90)
+    turtle.setheading(180)
     turtle.forward(50)
     turtle.stamp()
 
 def move_d():
-    turtle.right(90)
+    turtle.setheading(0)
     turtle.forward(50)
-    turtle.stamp()
-
-def move_s():
-    turtle.backward(50)
     turtle.stamp()
 
 turtle.shape('turtle')
 
-turtle.onkey(move_w, 'w')
-turtle.onkey(move_a, 'a')
-turtle.onkey(move_d, 'd')
-turtle.onkey(move_s, 's')
+
+
+turtle.onkey(move_w,'w')
+turtle.listen()
+turtle.onkey(move_a,'a')
+turtle.listen()
+turtle.onkey(move_s,'s')
+turtle.listen()
+turtle.onkey(move_d,'d')
 
 def restart():
     turtle.reset()
 
-turtle.shape('turtle')
-
-turtle.onkey(restart,'Escape')
+turtle.onkey(restart, 'Escape')
 turtle.listen()
+
 
 turtle.exitonclick()
